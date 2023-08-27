b09d9cc6150b465525fae7a050c0d4f9bb86fde9
Nasser Haji <info.hashemabadi@gmail.com>
2023-08-27 23:31:59 +0300

    Update surveillance-cameraV1.sol

--- surveillance-cameraV1.sol
+++ surveillance-cameraV1.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
